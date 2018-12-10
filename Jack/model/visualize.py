82ae1a8703b4541a5f4717e527a05d145e195f50
jryzkns <xza194@sfu.ca>
2018-12-09 16:47:53 -0800

    3rd family of networks setup

--- Jack/model/visualize.py
+++ Jack/model/visualize.py
@@ -32,17 +32,17 @@ kpL2 = []
 with open("kpL2_acc.blacke","r") as f:
     kpL2= sanitizeTensors(f.readline()[1:-1].split(","))
 
-# kpang = []
-# with open("kpang_acc.blacke","r") as f:
-#     kpang= sanitizeTensors(f.readline()[1:-1].split(","))
+# mixed = []
+# with open("mixed_acc.blacke","r") as f:
+#     mixed= sanitizeTensors(f.readline()[1:-1].split(","))
 
-# kpangL1 = []
-# with open("kpangL1_acc.blacke","r") as f:
-#     kpangL1= sanitizeTensors(f.readline()[1:-1].split(","))
+# mixedL1 = []
+# with open("mixedL1_acc.blacke","r") as f:
+#     mixedL1= sanitizeTensors(f.readline()[1:-1].split(","))
 
-# kpangL2 = []
-# with open("kpangL2_acc.blacke","r") as f:
-#     kpangL2= sanitizeTensors(f.readline()[1:-1].split(","))
+# mixedL2 = []
+# with open("mixedL2_acc.blacke","r") as f:
+#     mixedL2= sanitizeTensors(f.readline()[1:-1].split(","))
 
 fig, ax = plt.subplots()
 
@@ -59,9 +59,9 @@ plt.plot(kpL1, alpha = overall_opacity, label="BLACkp L1")
 plt.plot(kpL2, alpha = overall_opacity, label="BLACkp L2")
 
 # # yellow for combined model
-# plt.plot(kpang,   alpha = overall_opacity, label="BLACkpangle")
-# plt.plot(kpangL1, alpha = overall_opacity, label="BLACkpangle L1")
-# plt.plot(kpangL2, alpha = overall_opacity, label="BLACkpangle L2")
+# plt.plot(mixed,   alpha = overall_opacity, label="BLACkpangle")
+# plt.plot(mixedL1, alpha = overall_opacity, label="BLACkpangle L1")
+# plt.plot(mixedL2, alpha = overall_opacity, label="BLACkpangle L2")
 
 plt.title("accuracies of BLAC networks over training")
 plt.xlabel("Epoch")
