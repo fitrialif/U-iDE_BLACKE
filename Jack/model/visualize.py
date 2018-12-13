ed2a79cc867314f7bbd006f7487140e68f624d62
Jack Zhou <xza194@sfu.ca>
2018-12-13 00:53:22 -0800

    sleeping now

--- Jack/model/visualize.py
+++ Jack/model/visualize.py
@@ -11,9 +11,9 @@ ang = []
 with open("ang_acc.blacke","r") as f:
     ang = sanitizeTensors(f.readline()[1:-1].split(","))
 
-angL1 = []
-with open("angL1_acc.blacke","r") as f:
-    angL1= sanitizeTensors(f.readline()[1:-1].split(","))
+# angL1 = []
+# with open("angL1_acc.blacke","r") as f:
+#     angL1= sanitizeTensors(f.readline()[1:-1].split(","))
 
 angL2 = []
 with open("angL2_acc.blacke","r") as f:
@@ -23,9 +23,9 @@ kp = []
 with open("kp.blacke","r") as f:
     kp= sanitizeTensors(f.readline()[1:-1].split(","))
 
-kpL1 = []
-with open("kpL1_acc.blacke","r") as f:
-    kpL1= sanitizeTensors(f.readline()[1:-1].split(","))
+# kpL1 = []
+# with open("kpL1_acc.blacke","r") as f:
+#     kpL1= sanitizeTensors(f.readline()[1:-1].split(","))
 
 kpL2 = []
 with open("kpL2_acc.blacke","r") as f:
@@ -35,9 +35,9 @@ mixed = []
 with open("mixed.blacke","r") as f:
     mixed= sanitizeTensors(f.readline()[1:-1].split(","))
 
-mixedL1 = []
-with open("mixedL1_acc.blacke","r") as f:
-    mixedL1= sanitizeTensors(f.readline()[1:-1].split(","))
+# mixedL1 = []
+# with open("mixedL1_acc.blacke","r") as f:
+#     mixedL1= sanitizeTensors(f.readline()[1:-1].split(","))
 
 mixedL2 = []
 with open("mixedL2_acc.blacke","r") as f:
@@ -49,17 +49,17 @@ overall_opacity = 0.6
 
 # red
 plt.plot(ang,   alpha = overall_opacity, label="BLACangle")
-plt.plot(angL1, alpha = overall_opacity, label="BLACangle L1")
+# plt.plot(angL1, alpha = overall_opacity, label="BLACangle L1")
 plt.plot(angL2, alpha = overall_opacity, label="BLACangle L2")
 
 # green
 plt.plot(kp,   alpha = overall_opacity, label="BLACkp")
-plt.plot(kpL1, alpha = overall_opacity, label="BLACkp L1")
+# plt.plot(kpL1, alpha = overall_opacity, label="BLACkp L1")
 plt.plot(kpL2, alpha = overall_opacity, label="BLACkp L2")
 
 # yellow for combined model
 plt.plot(mixed,   alpha = overall_opacity, label="BLACkpangle")
-plt.plot(mixedL1, alpha = overall_opacity, label="BLACkpangle L1")
+# plt.plot(mixedL1, alpha = overall_opacity, label="BLACkpangle L1")
 plt.plot(mixedL2, alpha = overall_opacity, label="BLACkpangle L2")
 
 plt.title("Test Accuracy of BLAC* Networks")
