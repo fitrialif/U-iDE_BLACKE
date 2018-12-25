2bc641771d04591b84619fd926c3f90f39839527
jryzkns <xza194@sfu.ca>
2018-12-24 23:41:36 -0800

    cleaned up

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# U-iDE_BLACK
+# U-iDE_BLACKE
 - Brought to you by: The Undergrad-ient Descent Expedition (U-iDE)
-- BLACK: Body Language Affect Classification Kernel
+- BLACK: Body Language Affect Classification by Keypoint Estimation
+- As seen here: https://www.researchgate.net/project/BLACKE
 - CMPT 419/726 Final Project
 
 ## Team members:
 - Jack Zhou
-- Eric Gao
-- Jame Bie
 - Karan Aujla
+- Jame Bie
+- Eric Gao
 - Insoo Rhee
+
+## Notes
+- Most of the content in the repo is now deleted, but the what is left is the 6 BLAC* networks that takes in keypoint information that Openpose gives with the BODY_25 model: https://github.com/CMU-Perceptual-Computing-Lab/openpose
---
+++ model/visualize.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+def sanitizeTensors(ls):
+    out = []
+    for i in range(len(ls)):
+            out.append(int(ls[i][-3:-1]))
+    return out
+
+ang = []
+with open("ang_acc.blacke","r") as f:
+    ang = sanitizeTensors(f.readline()[1:-1].split(","))
+
+# angL1 = []
+# with open("angL1_acc.blacke","r") as f:
+#     angL1= sanitizeTensors(f.readline()[1:-1].split(","))
+
+angL2 = []
+with open("angL2_acc.blacke","r") as f:
+    angL2= sanitizeTensors(f.readline()[1:-1].split(","))
+
+kp = []
+with open("kp.blacke","r") as f:
+    kp= sanitizeTensors(f.readline()[1:-1].split(","))
+
+# kpL1 = []
+# with open("kpL1_acc.blacke","r") as f:
+#     kpL1= sanitizeTensors(f.readline()[1:-1].split(","))
+
+kpL2 = []
+with open("kpL2_acc.blacke","r") as f:
+    kpL2= sanitizeTensors(f.readline()[1:-1].split(","))
+
+mixed = []
+with open("mixed.blacke","r") as f:
+    mixed= sanitizeTensors(f.readline()[1:-1].split(","))
+
+# mixedL1 = []
+# with open("mixedL1_acc.blacke","r") as f:
+#     mixedL1= sanitizeTensors(f.readline()[1:-1].split(","))
+
+mixedL2 = []
+with open("mixedL2_acc.blacke","r") as f:
+    mixedL2= sanitizeTensors(f.readline()[1:-1].split(","))
+
+fig, ax = plt.subplots()
+
+overall_opacity = 0.6
+
+# red
+plt.plot(ang,   alpha = overall_opacity, label="BLACangle")
+# plt.plot(angL1, alpha = overall_opacity, label="BLACangle L1")
+plt.plot(angL2, alpha = overall_opacity, label="BLACangle L2")
+
+# green
+plt.plot(kp,   alpha = overall_opacity, label="BLACkp")
+# plt.plot(kpL1, alpha = overall_opacity, label="BLACkp L1")
+plt.plot(kpL2, alpha = overall_opacity, label="BLACkp L2")
+
+# yellow for combined model
+plt.plot(mixed,   alpha = overall_opacity, label="BLACkpangle")
+# plt.plot(mixedL1, alpha = overall_opacity, label="BLACkpangle L1")
+plt.plot(mixedL2, alpha = overall_opacity, label="BLACkpangle L2")
+
+plt.title("Test Accuracy of BLAC* Networks")
+plt.xlabel("EPOCH")
+plt.ylabel("Accuracy in percentage (%)")
+plt.legend(loc = 4)
+ax.set_ylim(0,100)
+plt.show()
